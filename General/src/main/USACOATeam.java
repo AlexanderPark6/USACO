35e3267bccabfd36773c85d58e060573f999047f
Alexander Park <alexanderpark706@gmail.com>
2022-06-14 19:02:16 -0700

    USACOATeam Update

--- General/src/main/USACOATeam.java
+++ General/src/main/USACOATeam.java
@@ -1,29 +1,24 @@
 package main;
 
+import java.util.Scanner;
+
 public class USACOATeam {
 
 	public static void main(String[] args) {
-		int n = Integer.parseInt(System.console().readLine());
-		int takenProblems = 0;
+		Scanner in = new Scanner(System.in);
+		int ans = 0;
+		int n = in.nextInt();
 		for(int i = 0; i < n; i++)
 		{
-			int ans = 0;
-			int[] problem = new int[3];
-			problem[i] = Integer.parseInt(System.console().readLine());
-			for(int j = 0; j < problem.length; j++)
+			int a = in.nextInt();
+			int b = in.nextInt();
+			int c = in.nextInt();
+			if(a+b+c >= 2)
 			{
-				if(problem[i] == 1)
-				{
-					ans++;
-				}
-			}
-			if(ans >= 2)
-			{
-				takenProblems++;
+				ans += 1;
 			}
 		}
-		System.out.println(takenProblems);
-
+		System.out.println(ans);
 	}
 
 }
---
+++ General/src/main/USACOPromoCounting.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class USACOPromoCounting {
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(System.in);
+		int b1 = in.nextInt();
+		int b2 = in.nextInt();
+		int s1 = in.nextInt();
+		int s2 = in.nextInt();
+		int g1 = in.nextInt();
+		int g2 = in.nextInt();
+		int p1 = in.nextInt();
+		int p2 = in.nextInt();
+		
+		
+		int gp = p1 - p2;
+		int sg = (p2+g2) - (p1-g1);
+		int bg = (g2+s2) - (g1-s1);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
