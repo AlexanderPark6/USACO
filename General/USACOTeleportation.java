b43fbf505cd66c69afc6d8bfca7480e3bd9db512
Alexander Park <77214520+AlexanderPark6@users.noreply.github.com>
2022-06-14 17:49:14 -0700

    Add files via upload

---
+++ General/USACOTeleportation.java
@@ -0,0 +1,29 @@
+package main;
+import java.io.*;
+public class USACOTeleportation {
+
+	public static void main(String[] args) throws IOException {
+
+
+		BufferedReader in = new BufferedReader(new FileReader("input.txt"));
+		PrintWriter out = new PrintWriter("teleport.out");
+		
+		String[] input = in.readLine().split(" ");
+		int a = Integer.parseInt(input[0]);
+		int b = Integer.parseInt(input[1]);
+		int x = Integer.parseInt(input[2]);
+		int y = Integer.parseInt(input[3]);
+		
+		
+		int answer = Math.abs(a-b);
+		answer = Math.min(answer, Math.abs(a-x) + Math.abs(b-y));
+		answer = Math.min(answer, Math.abs(a-y) + Math.abs(b-x));
+		
+		System.out.println(answer);
+		in.close();
+		out.close();
+		
+	
+	}
+
+}
