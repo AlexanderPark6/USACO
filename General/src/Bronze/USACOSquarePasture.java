3be7a70c25bcb8e5201410fdad03b6aee1cf4ad6
Alexander Park <alexanderpark706@gmail.com>
2022-06-21 17:26:57 -0700

    Create USACOSquarePasture.java

---
+++ General/src/Bronze/USACOSquarePasture.java
@@ -0,0 +1,11 @@
+package Bronze;
+import java.io.*;
+public class USACOSquarePasture {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in new Scanner(new FileReader("square.in"));
+		PrintWriter out = new PrintWriter("square.out");
+	}
+
+}
